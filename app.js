e962b3f94b449f7572656d51e219fd1e76c15d0c
mackio-0 <minkko000@gmail.com>
2023-12-06 01:26:52 +0630

    Add the game function, First steps finish

--- app.js
+++ app.js
@@ -8,61 +8,87 @@ function getComputerSelection() {
 }
 
 const computerSelection = getComputerSelection();
-const playerSelection = "Rock";
 
-let playerSelectionLowerCase = playerSelection.toLowerCase();
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelectionLowerCase, computerSelection) {
   if (playerSelectionLowerCase === "rock" && computerSelection === "rock") {
     console.log("Draw, Play again!");
     // console.log("Computer : " + computerSelection);
     // console.log("Player : " + playerSelectionLowerCase);
     playRound();
+    return 0;
   } else if (
     playerSelectionLowerCase === "rock" &&
     computerSelection === "paper"
   ) {
     console.log("You win! Paper beat rock!");
+    return 2;
   } else if (
     playerSelectionLowerCase === "rock" &&
     computerSelection === "scissor"
   ) {
     console.log("I win! Rock beat scissor");
+    return 1;
   } else if (
     playerSelectionLowerCase === "paper" &&
     computerSelection === "paper"
   ) {
     console.log("Draw, Play again!");
     playRound();
+    return 0;
   } else if (
     playerSelectionLowerCase === "paper" &&
     computerSelection === "rock"
   ) {
     console.log("I win! Paper beat rock!");
+    return 1;
   } else if (
     playerSelectionLowerCase === "paper" &&
     computerSelection === "scissor"
   ) {
     console.log("You win! Scisoor beat paper");
+    return 2;
   } else if (
     playerSelectionLowerCase === "scissor" &&
     computerSelection === "scissor"
   ) {
     console.log("Draw, Play again!");
     playRound();
+    return 0;
   } else if (
     playerSelectionLowerCase === "scissor" &&
     computerSelection === "paper"
   ) {
     console.log("I win! Paper beat scissor!");
+    return 1;
   } else {
     console.log("You win! Rock beat scissor");
+    return 2;
   }
 }
 
 // console.log("Computer : " + computerSelection);
 // console.log("Player : " + playerSelectionLowerCase);
 
-playRound(playerSelectionLowerCase, computerSelection);
+function game() {
+  let scoreForPlayer = 0;
+  let scoreForComputer = 0;
+  let scorePerGame = 0;
 
-// getComputerSelection();
+  for (let i = 0; i <= 4; i++) {
+    const playerSelectionLowerCase = prompt(
+      "Choose one, Rock, Paper, Scissor?"
+    ).toLowerCase();
+    scorePerGame = playRound(playerSelectionLowerCase, computerSelection);
+    if (scorePerGame === 1) {
+      scoreForPlayer++;
+    } else if (scorePerGame === 2) {
+      scoreForComputer++;
+    }
+  }
+
+  let result =
+    scoreForPlayer > scoreForComputer ? "Player win!" : "Computer win!";
+  console.log(result);
+}
+
+game();
